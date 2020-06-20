795e0f0c2949a84cf5bf40c0574f7985422b1585
FernandoMartinez <fmartinez14@miners.utep.edu>
2020-06-20 16:17:45 -0600

    adding placeholders

--- src/main/python/UI/DBA_FrontEnd/Decision.py
+++ src/main/python/UI/DBA_FrontEnd/Decision.py
@@ -15,12 +15,15 @@ class Decision(QWidget):
         self.setWindowTitle("if Statement")
         self.layout = QGridLayout()
         self.col = 0
-        
+
         self.if_label = QLabel()
         self.if_label.setText(" if")
         self.layout.addWidget(self.if_label, 0, 0)
 
-        self.layout.addWidget(QLineEdit(), 1, self.col)
+        FirstVar = QLineEdit()
+        FirstVar.setPlaceholderText("My_Variable ")
+        self.layout.addWidget(FirstVar, 1, self.col)
+
         self.col+=1
         logical_ops_box= QComboBox()
         logical_ops = ["",">", "<", "=>", "<=", "==", "!="]
@@ -72,7 +75,7 @@ class Decision(QWidget):
         self.layout.addWidget(self.if_label, 0, self.col//2)
 
         self.parent().resize(self.layout.sizeHint()) #Added this for menu size change
-        
+
 
     def saveMethod(self):
         decision_properties = []
